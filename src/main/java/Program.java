4e3073d10479854d66fc3aa98c770735e96b4c99
zonetanichan <taniyama.yuki@ist.osaka-u.ac.jp>
2016-03-14 20:45:37 +0900

    final2

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,19 +1,13 @@
 import java.lang.Math;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Program {
-	static Map<Integer[], Integer> values = new HashMap<>();
-	public static int A(int x, int y) {
-		Integer[] d={x,y};
-		Integer m = values.get(d);
-	    if (m != null) { return m; }
+
+public static int A(int x, int y) {
 		// ここに関数Aを実装してください
 		// 返り値は整数相当の型であれば，int以外の型へ変更しても構いません．
+		//System.out.println("chk:X="+x+",Y="+y);
 		if (x == 0) {
-			values.put(d, y + 1);
 			return y + 1;
-			
 		} else{
 			if (y == 0) {
 				int a=new Program().A(x-1,1);
@@ -25,7 +19,10 @@ public class Program {
 				return b;
 			}
 		}
-	}  public static boolean isPrime(int n) {
+		// return 0;
+	}
+
+  public static boolean isPrime(int n) {
     //ここに素数判定プログラムを実装してください。
 	boolean sosu=true;   //(2)
 	for(int i=2;i<n;i++){   //(3)
